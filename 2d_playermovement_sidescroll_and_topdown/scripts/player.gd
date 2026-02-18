efd4b804616a4d8e536186966f20bea92d686241
عقیل نوروزی | Alan <76964438+LAMEK8206@users.noreply.github.com>
2026-02-18 05:57:19 -0800

    بازنویسی کامل کد برای استفاده راحت در پروژه های دیگر
    کد ها به صورت کامل باز نویسی شده
    نوع گرفتن کلید های ورودی تغییر کره است و قابل تنظیم از اینسپکتور میباشد
    کد حرکت و حرکت  تغییر کرده است
     روش جدید برای نمایش انیمیشن ها  استفاده شده
    در کل بهتر شده دیگه

--- 2d_playermovement_sidescroll_and_topdown/scripts/player.gd
+++ 2d_playermovement_sidescroll_and_topdown/scripts/player.gd
@@ -2,83 +2,85 @@ extends CharacterBody2D
 
 #دریافت جاذبه از گیم انجین
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-#این متغیر برای ضرب در مقادیر دیگر به منظور کوچک کردن مقادیر اصلی است
-const power :float = 50.0
+
+#گرفتن کلید های ورودی برای حرکت
+@export var move_key : Array [Key] = [KEY_W,KEY_S,KEY_A,KEY_D,KEY_SPACE]
+
 #مقادیر کنترل‌کننده‌ی بازیکن
-@export var check_player_mode :bool = true
-@export var move_player_in_tow_axic : bool =false
+@export var player_mode :bool = true
 @export var player_move_speed :float = 100.0
-@export var player_jump_power :float = 400.0
+@export var player_jump_power :float = 350.0
 
 #ساخت آبجکت از نود AnimationSprite2D برای پخش انیمیشن‌ها
 @onready var animated_sprite : AnimatedSprite2D = $AnimatedSprite2D
 
 func _ready() -> void:
 	pass
-	
-func _physics_process(delta: float) -> void:
-	#چک کردن حالت‌های بازیکن
-	if check_player_mode == true :
-		#اعمال جاذبه به بازیکن
-		velocity.y += gravity * delta 
-		
-		#حرکت دادن بازیکن به سمت چپ و راست
-		if Input.is_action_pressed("a_player_move_left") : 
-			velocity.x = -player_move_speed * power * delta
-			animated_sprite.flip_h = true
-			animated_sprite.play("a&d_move")
-		elif Input.is_action_just_released("a_player_move_left") : 
-			velocity.x = 0
-			animated_sprite.play("a&d_idel")
 
-		if Input.is_action_pressed("d_player_move_right") : 
-			velocity.x = player_move_speed * power * delta
-			animated_sprite.flip_h = false
-			animated_sprite.play("a&d_move")
-		elif Input.is_action_just_released("d_player_move_right") : 
+# چک جهت انیمیشن ها
+enum Direct{
+	non , up , down , left , right
+}
+# مشخص کردن مقدار اویه داریکت
+var direct : Direct = Direct.non 
+func _physics_process(delta: float) -> void:
+	
+	#دریافت جهت های حرکت 
+	var dirction = Vector2 (int(Input.is_key_pressed(move_key[3])) - int (Input.is_key_pressed(move_key[2])) ,
+							int (Input.is_key_pressed(move_key[1])) - int(Input.is_key_pressed(move_key[0])) )
+
+	#حرکت پلیر در حالت تاپ دون
+	if  player_mode == false : #چک کردن حالت
+		if dirction : 
+			if Input.is_key_pressed(move_key[3]) :
+				velocity.x = dirction.x * player_move_speed
+				animated_sprite.flip_h = false # چرخاندن انیمشن پلیر
+				animated_sprite.play("a&d_move") # کد اجرا کردن انیمشن
+				direct = Direct.right # دادن مقدار به متقیری که مقدار دایرکت را میگیر بقه هم مشابه همین هستند
+			if Input.is_key_pressed(move_key[2]) : 
+				velocity.x = dirction.x * player_move_speed
+				animated_sprite.flip_h = true
+				animated_sprite.play("a&d_move")
+				direct = Direct.left
+			if Input.is_key_pressed(move_key[0]) :
+				velocity.y = dirction.y *player_move_speed
+				animated_sprite.play("w_move")
+				direct = Direct.up
+			if Input.is_key_pressed(move_key[1]) : 
+				velocity.y = dirction.y * player_move_speed
+				animated_sprite.play("s_move")
+				direct = Direct.down
+		else :
 			velocity.x = 0
-			animated_sprite.play("a&d_idel")
-			
-		#اضافه کردن پرش به بازیکن
-		if is_on_floor() and Input.is_action_just_pressed("k_player_jump") : velocity.y += -player_jump_power * power *delta
-		
-	#چک کردن حالت‌های بازیکن
-	if check_player_mode == false :
-		#حرکت بازیکن در حالت تاپ‌داون به چهار جهت
-		if Input.is_action_pressed("a_player_move_left") : 
-			velocity.x = -player_move_speed * power * delta
-			animated_sprite.flip_h = true
-			animated_sprite.play("a&d_move")
-		elif Input.is_action_just_released("a_player_move_left") : 
-			velocity.x = 0
-			animated_sprite.play("a&d_idel")
-		
-		if Input.is_action_pressed("d_player_move_right") : 
-			velocity.x = player_move_speed * power * delta
-			animated_sprite.flip_h = false
-			animated_sprite.play("a&d_move")
-		elif Input.is_action_just_released("d_player_move_right") : 
-			velocity.x = 0
-			animated_sprite.play("a&d_idel")
-		
-		if Input.is_action_pressed("w_player_move_up") : 
-			velocity.y = -player_move_speed * power * delta
-			animated_sprite.play("w_move")
-		elif Input.is_action_just_released("w_player_move_up") : 
 			velocity.y = 0
-			animated_sprite.play("w_idel")
+			if direct != Direct.non: # چک میکنیم که دکمه ای دکمه ای از کیبرد فشار داده نشده و انیمیشن ایدل پیشفرض پلی میشود
+				animated_sprite.play("idel_" + str(direct))
+	
+	#حرکت کردن پلیر در حالت ساید اسکرول
+	if player_mode == true : 
 		
-		if Input.is_action_pressed("s_player_move_down") : 
-			velocity.y = player_move_speed * power * delta
-			animated_sprite.play("s_move")
-		elif Input.is_action_just_released("s_player_move_down") : 
-			velocity.y = 0
-			animated_sprite.play("s_idel")
-		
-		if move_player_in_tow_axic == true :
-			if Input.is_action_pressed("a_player_move_left") and Input.is_action_pressed("w_player_move_up") : velocity = Vector2.ZERO
-			elif Input.is_action_pressed("a_player_move_left") and Input.is_action_pressed("s_player_move_down") : velocity = Vector2.ZERO
-			elif Input.is_action_pressed("s_player_move_down") and Input.is_action_pressed("d_player_move_right") : velocity = Vector2.ZERO
-			elif Input.is_action_pressed("d_player_move_right") and Input.is_action_pressed("w_player_move_up") : velocity = Vector2.ZERO
-		
+		#ا اعمال جاذبه به پلیر
+		if not is_on_floor() : 
+			velocity.y += gravity * delta
+		# حرکت به سمت چپ و راست
+		if dirction : 
+			if Input.is_key_pressed(move_key[3]) :
+				velocity.x = dirction.x * player_move_speed
+				animated_sprite.flip_h = false
+				animated_sprite.play("a&d_move")
+				direct = Direct.right
+			if Input.is_key_pressed(move_key[2]) : 
+				velocity.x = dirction.x * player_move_speed
+				animated_sprite.flip_h = true
+				animated_sprite.play("a&d_move")
+				direct = Direct.left
+		else :
+			velocity.x = 0
+			if direct != Direct.non : 
+				animated_sprite.play("idel_" + str(direct))
+		# پریدن پلیر
+		if Input.is_key_pressed(move_key[4]) and is_on_floor() : 
+			velocity.y -= player_jump_power
+
+
 	move_and_slide()
